0d49a09cbdf1a438e6fdd3ca87a18c6ef8e35a97
Samit Koyom <samitkoyom@gmail.com>
2019-01-28 17:18:13 +0700

    update track 2

--- src/app/firebase.credentials.ts
+++ src/app/firebase.credentials.ts
@@ -1,8 +1,8 @@
 export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyDDf8z2CWPiif8w4HT7erOOW3E2xM-cTUM",
-    authDomain: "fir-testsamit.firebaseapp.com",
-    databaseURL: "https://fir-testsamit.firebaseio.com",
-    projectId: "fir-testsamit",
-    storageBucket: "fir-testsamit.appspot.com",
-    messagingSenderId: "363297773901"
+    apiKey: "xxx",
+    authDomain: "xxx",
+    databaseURL: "xxx",
+    projectId: "xxx",
+    storageBucket: "xxx",
+    messagingSenderId: "xx"
 };
