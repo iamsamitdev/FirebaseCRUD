941bc9c5c0df357c028e950f25eb2713374ba8a0
Samit Koyom <samitkoyom@gmail.com>
2019-01-28 17:16:28 +0700

    initial track

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDDf8z2CWPiif8w4HT7erOOW3E2xM-cTUM",
+    authDomain: "fir-testsamit.firebaseapp.com",
+    databaseURL: "https://fir-testsamit.firebaseio.com",
+    projectId: "fir-testsamit",
+    storageBucket: "fir-testsamit.appspot.com",
+    messagingSenderId: "363297773901"
+};
